bad8f57aa310219d85cceb89ba16e28c0f2db694
Sakshi Singh <sakshiisingh.0528@gmail.com>
2026-06-03 01:15:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const display = document.getElementById("display");
+
+function appendValue(value){
+
+    display.value += value;
+}
+
+function clearDisplay(){
+
+    display.value = "";
+}
+
+function deleteLast(){
+
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+
+    try{
+
+        display.value = eval(display.value);
+    }
+
+    catch{
+
+        display.value = "Error";
+    }
+}
+
+document.addEventListener("keydown",(event)=>{
+
+    const key = event.key;
+
+    if(!isNaN(key) || "+-*/.%".includes(key)){
+
+        appendValue(key);
+    }
+
+    else if(key === "Enter"){
+
+        calculate();
+    }
+
+    else if(key === "Backspace"){
+
+        deleteLast();
+    }
+
+    else if(key === "Escape"){
+
+        clearDisplay();
+    }
+});
